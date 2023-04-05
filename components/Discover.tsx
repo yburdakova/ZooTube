eaba984add4cd26dd6bf8347eda621d865be85d4
Yana Burdakova <bu.asti@gmail.com>
2023-04-05 15:28:23 -0400

    Project completed

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -13,16 +13,16 @@ const Discover = () => {
     const topicsStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 cursor-pointer text-black justify-center';
     
     return (
-        <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
-            <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">Popular topics</p>
-            <div className="flex gap-3 flex-wrap justify-center">
+        <div className='pb-6 xl:border-b-2 xl:border-gray-200'>
+            <p className="hidden m-3 mt-4 font-semibold text-gray-500 xl:block">Popular topics</p>
+            <div className="flex flex-wrap gap-3 ">
                 {topics.map((item) => (
                     <Link href={`/?topic=${item.name}`} key={item.name}>
                         <div className={topic === item.name ? activeTopicStyle : topicsStyle}>
-                            <span className="font-bold text-2x xl:text-md">
+                            <span className="text-2xl font-bold xl:text-md">
                                 {item.icon}
                             </span>
-                            <span className="font-medium text-md hidden xl:block capitalize">
+                            <span className="hidden font-medium capitalize text-md xl:block">
                                 {item.name}
                             </span>
                         </div>
